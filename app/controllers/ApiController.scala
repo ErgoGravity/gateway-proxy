1e0dcf2081f2d4825d5a5222963eca9bb9ab7b3b
moein zargarzadeh <moein.zargarzadeh@gmail.com>
2021-05-12 12:21:25 +0000

    Add pulse

--- app/controllers/ApiController.scala
+++ app/controllers/ApiController.scala
@@ -4,9 +4,12 @@ import javax.inject._
 import network.{Client, Explorer}
 import play.api.Logger
 import play.api.mvc._
-import helpers.{Configs, Utils}
+import helpers.Utils
 import io.circe.Json
+import io.circe.syntax._
 import play.api.libs.circe.Circe
+import gateway.Adaptor
+import org.ergoplatform.appkit.JavaHelpers
 
 import scala.concurrent.ExecutionContext
 
@@ -14,7 +17,7 @@ import scala.concurrent.ExecutionContext
  * Controller of Gravity-Ergo-Proxy.
  */
 class ApiController @Inject()(controllerComponents: ControllerComponents,
-                              client: Client, explorer: Explorer, utils: Utils)
+                              client: Client, explorer: Explorer, utils: Utils, adaptor: Adaptor)
                              (implicit ec: ExecutionContext) extends AbstractController(controllerComponents) with Circe {
 
   private val logger: Logger = Logger(this.getClass)
@@ -69,7 +72,7 @@ class ApiController @Inject()(controllerComponents: ControllerComponents,
     try {
       val msg = request.body.hcursor.downField("msg").as[String].getOrElse(throw new Throwable("msg field must exist"))
       val sk = request.body.hcursor.downField("sk").as[String].getOrElse(throw new Throwable("sk field must exist"))
-      val signed = client.sign(msg, sk)
+      val signed = adaptor.sign(msg, sk)
       Ok(
         s"""{
            |  "success": true,
@@ -101,7 +104,7 @@ class ApiController @Inject()(controllerComponents: ControllerComponents,
       Ok(
         s"""{
            |  "success": true,
-           |  "verified": ${client.verify(msg, signed_a, signed_z, pk)}
+           |  "verified": ${adaptor.verify(msg, signed_a, signed_z, pk)}
            |}""".stripMargin
       ).as("application/json")
 
@@ -122,7 +125,7 @@ class ApiController @Inject()(controllerComponents: ControllerComponents,
       Ok(
         s"""{
            |  "success": true,
-           |  "pk": "${client.getPkFromSk(sk)}"
+           |  "pk": "${adaptor.getPkFromSk(sk)}"
            |}""".stripMargin
       ).as("application/json")
 
@@ -130,5 +133,75 @@ class ApiController @Inject()(controllerComponents: ControllerComponents,
       case e: Throwable => exception(e)
     }
   }
+
+  /**
+   * @return Last Pulse Id
+   */
+  def getLastPulseId: Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    try {
+      Ok(
+        s"""{
+           |  "success": true,
+           |  "lastPulseId": ${adaptor.getLastPulseId}
+           |}""".stripMargin
+      ).as("application/json")
+
+    } catch {
+      case e: Throwable => exception(e)
+    }
+  }
+
+  /**
+   * @return Last oracles and bft value
+   */
+  def getPreAddPulseInfo: Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    try {
+      val preAddPulseInfo = adaptor.getPreAddPulseInfo
+      Ok(
+        s"""{
+           |  "success": true,
+           |  "bft": ${preAddPulseInfo._2},
+           |  "oracles": ${preAddPulseInfo._1.asJson}
+           |}""".stripMargin
+      ).as("application/json")
+
+    } catch {
+      case e: Throwable => exception(e)
+    }
+  }
+
+  /**
+   * Add a pulse TX
+   * expects :
+   *      {
+   *        "hashData" : "f9e5ce5aa0d95f5d54a7bc89c46730d9662397067250aa18a0039631c0f5b809",
+   *        "signs": {
+   *          "a": ["9662397067250aa18a0039631c0f5b809", "9662397067250aa18a0039631c0f5b809", "", "9662397067250aa18a0039631c0f5b809", "9662397067250aa18a0039631c0f5b809"],
+   *          "z": ["78132684712638457631278", "78132684712638457631278", "0", "78132684712638457631278", "78132684712638457631278"]
+   *        }
+   *      }
+   *
+   * @return tx id
+   */
+  def addPulse(): Action[Json] = Action(circe.json) { implicit request =>
+    try {
+      val hashData = request.body.hcursor.downField("hashData").as[String].getOrElse(throw new Throwable("hashData field must exist"))
+      val signs = request.body.hcursor.downField("signed").as[Json].getOrElse(throw new Throwable("signs fields must exist"))
+      val listSigns_a = signs.hcursor.downField("a").as[Seq[String]].getOrElse(throw new Throwable("signed fields a must exist"))
+      val listSigns_z = signs.hcursor.downField("z").as[Seq[String]].getOrElse(throw new Throwable("signed fields z must exist")).map(BigInt(_).bigInteger)
+
+      if (listSigns_a.size != 5 || listSigns_z.size != 5) throw new Throwable("in signed fields a and z must be 5 object")
+      Ok(
+        s"""{
+           |  "success": true,
+           |  "txId": "${adaptor.addPulse(utils.toByteArray(hashData), (listSigns_a.map(utils.hexToGroupElement).toArray, listSigns_z.map(JavaHelpers.SigmaDsl.BigInt(_)).toArray))}"
+           |}""".stripMargin
+      ).as("application/json")
+
+    } catch {
+      case e: Throwable => exception(e)
+    }
+  }
+
 }
 
--- app/network/Explorer.scala
+++ app/network/Explorer.scala
@@ -4,35 +4,37 @@ import helpers.Configs
 import io.circe.Json
 import io.circe.parser.parse
 import javax.inject.Singleton
-import scalaj.http.Http
+import scalaj.http.{BaseHttp, HttpConstants}
 
+import scala.util.{Failure, Success, Try}
 
 @Singleton
 class Explorer() {
-  private val baseUrl = s"${Configs.explorerUrl}/api/v0"
-  private val tx = s"$baseUrl/transactions"
-  private val unconfirmedTx = s"$baseUrl/transactions/unconfirmed"
-
-  private val defaultHeader: Seq[(String, String)] = Seq[(String, String)](("Content-Type", "application/json"))
+  private val baseUrlV0 = s"${Configs.explorerUrl}/api/v0"
+  private val baseUrlV1 = s"${Configs.explorerUrl}/api/v1"
+  private val tx = s"$baseUrlV0/transactions"
+  private val unconfirmedTx = s"$baseUrlV0/transactions/unconfirmed"
 
   /**
    * @param txId transaction id
    * @return transaction if it is unconfirmed
    */
-  def getUnconfirmedTx(txId: String): Json = {
-    val unconfirmed = Http(s"$unconfirmedTx/$txId").headers(defaultHeader).asString
-    if (unconfirmed.isError) Json.Null
-    else parse(unconfirmed.body).getOrElse(Json.Null)
+  def getUnconfirmedTx(txId: String): Json = try {
+    GetRequest.httpGet(s"$unconfirmedTx/$txId")
+  }
+  catch {
+    case _: Throwable => Json.Null
   }
 
   /**
    * @param txId transaction id
    * @return transaction if it is confirmed (mined)
    */
-  def getConfirmedTx(txId: String): Json = {
-    val confirmed = Http(s"$tx/$txId").headers(defaultHeader).asString
-    if (confirmed.isError) Json.Null
-    else parse(confirmed.body).getOrElse(Json.Null)
+  def getConfirmedTx(txId: String): Json = try {
+    GetRequest.httpGet(s"$tx/$txId")
+  }
+  catch {
+    case _: Throwable => Json.Null
   }
 
   /**
@@ -47,6 +49,33 @@ class Explorer() {
       if (conf != Json.Null) conf.hcursor.downField("summary").as[Json].getOrElse(Json.Null)
         .hcursor.downField("confirmationsCount").as[Int].getOrElse(-1)
       else -1
+    }
+  }
+
+}
+
+object GetRequest{
+  object GravityHttp extends BaseHttp (None, HttpConstants.defaultOptions, HttpConstants.utf8, 4096, "Mozilla/5.0 (X11; OpenBSD amd64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36",
+  true
+  )
+  private val defaultHeader: Seq[(String, String)] = Seq[(String, String)](("Accept", "application/json"))
+  def httpGetWithError(url: String, headers: Seq[(String, String)] = defaultHeader): Either[Throwable, Json] = {
+    Try {
+      val responseReq = GravityHttp(url).headers(defaultHeader).asString
+      println(responseReq.body)
+      (responseReq.code, responseReq)
+    }
+    match{
+      case Success((200, responseReq)) => parse(responseReq.body)
+      case Success((responseHttpCode, responseReq)) => Left(new Exception(s"returned a error with http code $responseHttpCode and error ${responseReq.throwError}"))
+      case Failure(exception) => Left(exception)
+    }
+  }
+
+  def httpGet(url: String): Json = {
+    httpGetWithError(url) match {
+      case Right(json) => json
+      case Left(ex) => throw ex
     }
   }
 }
