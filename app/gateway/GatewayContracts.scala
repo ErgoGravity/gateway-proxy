cd3286bb9f89da603e632efb006725964d666ef9
Moein zargarzadeh <moein.zargarzadeh@gmail.com>
2021-06-04 23:27:08 +0430

    change pulse contract for valueToSub state

--- app/gateway/GatewayContracts.scala
+++ app/gateway/GatewayContracts.scala
@@ -79,6 +79,9 @@ class GatewayContracts @Inject()(client: Client) {
   lazy val signalScript: String =
     s"""{
        | sigmaProp(allOf(Coll(
+       |  // To prevent placing two signal boxes in one transaction
+       |  SELF.id == INPUTS(0).id,
+       |
        |  // There must be a msgHash in the R4 of the signal box
        |  SELF.R4[Coll[Byte]].isDefined,  // TODO: In the future, we have to check the msgHash for the USER-SC.
        |
@@ -134,8 +137,8 @@ class GatewayContracts @Inject()(client: Client) {
        | // We expect second option of signs to be in R7 [z, z, ..]
        | val signs_z = OUTPUTS(0).R6[Coll[BigInt]].get
        |
-       | // should to be box of oracle contract
-       | val dataInput = CONTEXT.dataInputs(0)
+       | val currentPulseId = SELF.R7[Long].get
+       | val signalCreated: Boolean = SELF.R8[Boolean].get
        |
        | // Verify signs
        | val validateSign: Int = {(v: ((Coll[Byte], GroupElement), (GroupElement, BigInt))) => {
@@ -147,48 +150,84 @@ class GatewayContracts @Inject()(client: Client) {
        |    if (l == r) 1 else 0
        | }}
        |
-       | // We Expect number of oracles that verified msgHash of in pulseId bigger than bftValue
-       | val check_bftCoefficient = {
-       |   // We expect one tokenNFT for oracle contract to be in token(0) of this box
-       |   if (dataInput.tokens(0)._1 == oracleNebulaNFT) {
-       |     // get BftCoefficient from R4 of oracleContract Box
-       |     val bftValue = dataInput.R4[Int].get
-       |     // Get oracles from R5 of oracleContract Box and convert to Coll[GroupElement]
-       |     val oracles: Coll[GroupElement] = dataInput.R5[Coll[Coll[Byte]]].get.map({ (oracle: Coll[Byte]) =>
-       |         decodePoint(oracle)
-       |     })
-       |     val count : Int= validateSign(((msgHash, oracles(0)),(signs_a(0), signs_z(0)))) + validateSign(((msgHash, oracles(1)),(signs_a(1), signs_z(1)))) + validateSign(((msgHash, oracles(2)),(signs_a(2), signs_z(2)))) + validateSign(((msgHash, oracles(3)),(signs_a(3), signs_z(3)))) + validateSign(((msgHash, oracles(4)),(signs_a(4), signs_z(4))))
-       |     count >= bftValue
-       |   }
-       |  else false
-       | }
-       |
-       | val checkOUTPUTS = {
-       |   if(SELF.tokens(0)._1 == pulseNebulaNFT) {
+       | val publicCheckOutBoxes: Boolean = {(box: (Box, Box)) => {
        |    allOf(Coll(
        |      // We expect one tokenNFT for pulse contract to be in token(0)
-       |      OUTPUTS(0).tokens(0)._1 == pulseNebulaNFT,
+       |      box._2.tokens(0)._1 == pulseNebulaNFT,
        |      // Value of new pulse box must be greater than equal to value of pulse box in input
-       |      OUTPUTS(0).value >= SELF.value,
+       |      box._2.value >= box._1.value,
        |      // Contract of new pulse box must be equal to contract of pulse box in input
-       |      OUTPUTS(0).propositionBytes == SELF.propositionBytes,
-       |      // We expect pulseId to be in R7 and increase pulseId in out box
-       |      OUTPUTS(0).R7[Long].get == SELF.R7[Long].get + 1,
-       |
-       |      // Contract of second INPUT/OUTPUT must be equal to tokenRepoContractHash
-       |      blake2b256(INPUTS(1).propositionBytes) == tokenRepoContractHash,
-       |      blake2b256(OUTPUTS(1).propositionBytes) == tokenRepoContractHash,
-       |
-       |      // Contract of third OUTPUT must be equal to signalContractHash
-       |      blake2b256(OUTPUTS(2).propositionBytes) == signalContractHash,
-       |      // There must be a msgHash in the R4 of the signal box
-       |      OUTPUTS(2).R4[Coll[Byte]].get == msgHash
+       |      box._2.propositionBytes == box._1.propositionBytes
        |    ))
-       |   }
-       |   else false
-       | }
+       | }}
        |
-       | sigmaProp ( check_bftCoefficient && checkOUTPUTS )
+       | val verified = if (signalCreated) {
+       |    // should to be box of oracle contract
+       |    val dataInput = CONTEXT.dataInputs(0)
+       |    // We Expect number of oracles that verified msgHash of in pulseId bigger than bftValue
+       |    val check_bftCoefficient = {
+       |      // We expect one tokenNFT for oracle contract to be in token(0) of this box
+       |      if (dataInput.tokens(0)._1 == oracleNebulaNFT) {
+       |        // get BftCoefficient from R4 of oracleContract Box
+       |        val bftValue = dataInput.R4[Int].get
+       |        // Get oracles from R5 of oracleContract Box and convert to Coll[GroupElement]
+       |        val oracles: Coll[GroupElement] = dataInput.R5[Coll[Coll[Byte]]].get.map({ (oracle: Coll[Byte]) =>
+       |            decodePoint(oracle)
+       |        })
+       |        val count : Int= validateSign(((msgHash, oracles(0)),(signs_a(0), signs_z(0)))) + validateSign(((msgHash, oracles(1)),(signs_a(1), signs_z(1)))) + validateSign(((msgHash, oracles(2)),(signs_a(2), signs_z(2)))) + validateSign(((msgHash, oracles(3)),(signs_a(3), signs_z(3)))) + validateSign(((msgHash, oracles(4)),(signs_a(4), signs_z(4))))
+       |        count >= bftValue
+       |      }
+       |     else false
+       |    }
+       |    val checkOUTPUTS = {
+       |     if(SELF.tokens(0)._1 == pulseNebulaNFT) {
+       |      allOf(Coll(
+       |        publicCheckOutBoxes((SELF, OUTPUTS(0))),
+       |        // We expect pulseId to be in R7 and increase pulseId in out box
+       |        OUTPUTS(0).R7[Long].get == currentPulseId + 1,
+       |        OUTPUTS(0).R8[Boolean].get == false
+       |      ))
+       |     }
+       |     else false
+       |    }
+       |    (check_bftCoefficient && checkOUTPUTS)
+       |  } else {
+       |     val checkRegisters = {
+       |      allOf(Coll(
+       |         SELF.R4[Coll[Byte]].get == msgHash,
+       |         SELF.R5[Coll[GroupElement]].get == signs_a,
+       |         SELF.R6[Coll[BigInt]].get == signs_z,
+       |         // We expect pulseId to be in R7 and increase pulseId in out box
+       |         OUTPUTS(0).R7[Long].get == currentPulseId,
+       |         OUTPUTS(0).R8[Boolean].get == true,
+       |
+       |         // Expect pulseId to be in R4 of the signal box
+       |         OUTPUTS(2).R4[Long].get == currentPulseId,
+       |         // There must be data in the R5 of the signal box
+       |         // TODO: this data must be equal to msgHash
+       |         OUTPUTS(2).R5[Coll[Byte]].isDefined
+       |      ))
+       |     }
+       |     val checkOUTPUTS = {
+       |       if(SELF.tokens(0)._1 == pulseNebulaNFT) {
+       |        allOf(Coll(
+       |          publicCheckOutBoxes((SELF, OUTPUTS(0))),
+       |
+       |          // Contract of second INPUT/OUTPUT must be equal to tokenRepoContractHash
+       |          blake2b256(INPUTS(1).propositionBytes) == tokenRepoContractHash,
+       |          blake2b256(OUTPUTS(1).propositionBytes) == tokenRepoContractHash,
+       |
+       |          // Contract of third OUTPUT must be equal to signalContractHash
+       |          blake2b256(OUTPUTS(2).propositionBytes) == signalContractHash
+       |        ))
+       |       }
+       |       else false
+       |    }
+       |    (checkRegisters && checkOUTPUTS)
+       |  }
+       |
+       |
+       | sigmaProp ( verified )
        |
        | }
     """.stripMargin
